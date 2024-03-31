f52ef604c939c0162203ebaa3cfd9525a921060b
Deepak babu <85184074+deepaktechnical007@users.noreply.github.com>
2024-04-01 02:55:24 +0530

    Using Spring Initializr

---
+++ dpkcourse-api/src/test/java/io/dpk/springquickstart/dpkcourseapi/DpkcourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dpk.springquickstart.dpkcourseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpkcourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
